d2607e86b15c25bf0431c0beed91949de32091cc
adn0n <adnanakram.2020@vemanait.edu.in>
2025-01-28 17:36:10 +0530

    added tokenomics section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'morris': ['Morris', 'serif'],
+			},
+			animation: {
+				'glow': 'glow 4s ease-in-out infinite',
+				'pulse': 'pulse 4s cubic-bezier(0.2, 0, 0.3, 1) infinite',
+			},
+			keyframes: {
+				glow: {
+					'0%, 100%': {
+						opacity: 1,
+					},
+					'50%': {
+						opacity: 0.5,
+					},
+				},
+			},
+		},
 	},
 	plugins: [],
 }
